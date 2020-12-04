7d7a97c512e8f4546287400c51b18af6b7d10182
yujin211 <kyujin0211@naver.com>
2020-12-05 02:06:05 +0900

    캡스톤 기말/졸업 과제

---
+++ app/src/main/java/com/example/application/MovieListResult.java
@@ -0,0 +1,12 @@
+package com.example.application;
+
+import java.util.ArrayList;
+
+public class MovieListResult {
+
+    String boxofficeType;
+    String showRange;
+
+    ArrayList<Movie> dailyBoxOfficeList = new ArrayList<Movie>();
+
+}
